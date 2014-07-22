eb517f80d3bee68cd4a294b560d738a5a845ef60
Erik Lieben <erik.lieben@gmail.com>
2014-07-22 07:07:56 +0200

    Add unit of work factory

---
+++ ErikLieben.Data/Repository/IUnitOfWorkFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace ErikLieben.Data.Repository
+{
+    public interface IUnitOfWorkFactory
+    {
+        IUnitOfWorkAsync Create<T>() where T : class;
+
+        IUnitOfWorkAsync CreateAsync<T>() where T : class;
+    }
+}
