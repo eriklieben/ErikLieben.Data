1538cf40de821076402669bcfd6a8c00972d2687
Erik Lieben <erik.lieben@gmail.com>
2014-07-23 07:33:45 +0200

    allow casted IQuerable as IEnumerable to be projected

--- ErikLieben.Data/Projection/IQueryableExtensions.cs
+++ ErikLieben.Data/Projection/IQueryableExtensions.cs
@@ -1,5 +1,7 @@
 ﻿namespace ErikLieben.Data.Projection
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     /// <summary>
@@ -10,6 +12,17 @@
         public static ProjectionExpression<TSource> Project<TSource>(this IQueryable<TSource> source)
         {
             return new ProjectionExpression<TSource>(source);
+        }
+
+        public static ProjectionExpression<TSource> Project<TSource>(this IEnumerable<TSource> source)
+        {
+            var result = source as IQueryable<TSource>;
+            if (result == null)
+            {
+                throw new ArgumentException("source is not IQueryable<TSource>");
+            }
+
+            return new ProjectionExpression<TSource>(result);
         }
     }
 }
